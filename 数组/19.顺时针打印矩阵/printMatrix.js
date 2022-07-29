55480112cfe981739784ae1119ee50e3a48ff93d
rrh <1461694018@qq.com>
2022-07-29 19:34:52 +0800

    feat: 顺时针打印矩阵

--- 数组/19.顺时针打印矩阵/printMatrix.js
+++ 数组/19.顺时针打印矩阵/printMatrix.js
@@ -1,24 +1,16 @@
 /*
 输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字
 例如，如果输入如下4 X 4矩阵： 
-    1  2  3  4 
-    5  6  7  8 
-    9  10 11 12 
-    13 14 15 16 
+    1  2  3  4  5     0 
+    6  7  8  9  10 
+    11 12 13 14 15 
+    16 17 18 19 20
 则依次打印出数字
     1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
 */
 // 左->右，上->下 右->左 下->上
 function printMatrix(matrix) {
-    // write code here
-    let row = matrix.length
-    if (row === 0) {
-        return [].join('')
-    }
-    let col = matrix[0].length
-    if (col === 0) {
-        return [].join('')
-    }
+
     let res = [],
         start = 0
     while (start * 2 < row && start * 2 < col) {
@@ -49,46 +41,9 @@ function print(matrix, row, col, start, res) {
     }
 }
 
-
-
-//始终移除第一行 + 变相转置
-function printMatrix2(matrix) {
-    let row = matrix.length
-    if (row === 0) {
-        return [].join('')
-    }
-    let col = matrix[0].length
-    if (col === 0) {
-        return [].join('')
-    }
-    let res = []
-    while (matrix.length) {
-        let firstRow = matrix.shift()
-        res.push(...firstRow)
-        matrix = transpose(matrix)
-    }
-    return res.join(',')
-}
-
-function transpose(matrix) {
-    if (!matrix.length) {
-        return []
-    }
-    let res = [],
-        col = matrix.length,
-        row = matrix[0].length
-    for (let i = row - 1; i >= 0; i--) {
-        let tmp = []
-        for (let j = 0; j < col; j++) {
-            tmp.push(matrix[j][i])
-        }
-        res.push(tmp)
-    }
-    return res
-}
 var matrix = [],
     row = 4,
-    col = 4
+    col = 5
 for (let i = 1; i <= row; i++) {
     let arr = []
     for (let j = 1; j <= col; j++) {
@@ -96,4 +51,4 @@ for (let i = 1; i <= row; i++) {
     }
     matrix.push(arr)
 }
-console.log(printMatrix2(matrix))
+console.log(printMatrix(matrix))
---
+++ 数组/19.顺时针打印矩阵/spiralOrder.ts
@@ -0,0 +1,61 @@
+/**
+ * 题目描述：
+ *       输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字。
+ * 分析：
+ *      分层遍历，每层按从左到右，从上到下，从右到左，从下到上进行遍历打印。从 0层开始，每打印完一层行和列均减去2，因此层数 * 2应该同时小于行数与列数，注意打印到最后一层，如果只剩下一行，则不需要从右到左，同理，只剩下一列，则不需要从下到上。
+ *      移除首行 + 变相转置（向左翻转），先将矩阵的第一行打印，对剩下的矩阵进行向左翻转（变相转置），依次打印翻转后的第一行，直到矩阵为空。
+*/
+
+// 按层遍历
+function spiralOrder1(matrix: number[][]): number[] {
+    const row = matrix?.length;
+    const col = matrix?.[0]?.length;
+    let layer = 0;
+    const printResult: number[] = [];
+    while (layer * 2 < row && layer * 2 < col) { // 每遍历一次，行和列均减去了2，
+        const endRowIndex = row - 1 - layer;
+        const endColIndex = col - 1 - layer;
+        for (let j = layer; j <= endColIndex; j++) {  // 从左向右
+            printResult.push(matrix[layer][j]);
+        }
+        for (let i = layer + 1; i <= endRowIndex; i++) { // 从上到下 
+            printResult.push(matrix[i][endColIndex]);
+        }
+        if (layer < endRowIndex) {  // 排除只剩下一行，不需要从右到左
+            for (let i = endColIndex - 1; i >= layer; i--) { // 从右到左
+                printResult.push(matrix[endRowIndex][i]);
+            }
+        }
+        if (layer < endColIndex) {  // 排除只剩下一列，不需要从下到上
+            for (let j = endRowIndex - 1; j > layer; j--) { // 从下到上
+                printResult.push(matrix[j][layer]);
+            }
+        }
+        layer = layer + 1;
+    }
+    return printResult;
+};
+
+// 移除首行 + 变相转置（向左翻转）
+function spiralOrder2(matrix: number[][]): number[] {
+    const printResult: number[] = [];
+    function transpose(matrix: number[][]): number[][] {
+        const transposedMatrix: number[][] = [];
+        const row = matrix?.length;
+        const col = matrix?.[0]?.length;
+        for (let j = col - 1; j >= 0; j--) {
+            const tmp: number[] = [];
+            for (let i = 0; i < row; i++) {
+                tmp.push(matrix[i][j]);
+            }
+            transposedMatrix.push(tmp);
+        }
+        return transposedMatrix;
+    }
+    while (matrix.length) {
+        const firstRow = matrix.shift() as number[];
+        printResult.push(...firstRow); // 每次将第一行放入结果数组中
+        matrix = transpose(matrix); // 对矩阵进行变相转置（向左翻转）
+    }
+    return printResult;
+};
