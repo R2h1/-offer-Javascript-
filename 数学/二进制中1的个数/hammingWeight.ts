c14a70904d832b90835411b7c8dea0060267bddd
rrh <1461694018@qq.com>
2022-08-19 17:57:29 +0800

    feat: 二进制中1的个数

--- 数学/二进制中1的个数/hammingWeight.ts
+++ 数学/二进制中1的个数/hammingWeight.ts
@@ -3,10 +3,10 @@
  *      编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为 汉明重量).）。
  *  分析：
  *      循环检查二进制位法，当检查第 i位时，我们可以让 n 与 2^i 进行与运算，当且仅当 n 的第 i 位为 1 时，运算结果不为 0。
- *      位运算法，n & (n−1)会将n 的最右边的那个1变成0，直到n 等于0，运算次数就等于 n 的二进制位中 1 的个数。
+ *      位运算优化法，n & (n−1)会将n 的最右边的那个1变成0，直到n 等于0，运算次数就等于 n 的二进制位中 1 的个数。
  */
 
-// 位运算
+// 位运算优化法
 function hammingWeight1(n: number): number {
     let count = 0
     while (n !== 0) {
@@ -17,7 +17,7 @@ function hammingWeight1(n: number): number {
     return count;
 }
 
-/*利用位运算，直接操作二进制*/
+// 循环检查二进制位法
 function hammingWeight2(n: number): number {
     let count = 0;
     for (let i = 0; i < 32; i++) {
