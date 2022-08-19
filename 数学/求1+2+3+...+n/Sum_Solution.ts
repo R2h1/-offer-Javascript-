bfa8fcab7bc4c481ebcf40f51b9d589dee04d2a8
rrh <1461694018@qq.com>
2022-08-19 20:50:09 +0800

    feat: 求1+2+3+...+n

---
+++ 数学/求1+2+3+...+n/Sum_Solution.ts
@@ -0,0 +1,13 @@
+/**
+ * 题目描述：
+ *      求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ *  分析：
+ *      递归法，使用短路表达式，当 n !== 0 的时候进行递归累加
+ */
+
+// 短路递归法
+function sumNums(n: number): number {
+    let sum = n;
+    n && (sum = sum + sumNums(n - 1));
+    return sum;
+};
