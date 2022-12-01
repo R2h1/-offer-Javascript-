c49b42331ae0b353f40ea93c85e0cd407b6bba6f
R2h1 <1461694018@qq.com>
2022-12-01 18:28:55 +0800

    feat: 添加和数组相关的工具函数

---
+++ interview/zaihu.js
@@ -0,0 +1,42 @@
+function Action(name) {
+  this.name = name;
+  this.queue = [() => console.log(`start ${this.name}`)];
+
+}
+
+Action.prototype = {
+  constructor: Action,
+  do(action) {
+    this.queue.push(() => console.log(`${this.name} ${action}`));
+    return this;
+  },
+  wait(delay) {
+    this.queue.push(() => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(() => console.log(`wait ${delay}s`))
+        }, delay * 1000);
+      })
+    });
+    return this;
+  },
+  waitFirst(delay) {
+    this.queue.unshift(() => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(() => console.log(`wait ${delay}s`))
+        }, delay * 1000);
+      })
+    });
+    return this;
+  },
+  async execute() {
+    for (let i = 0; i < this.queue.length; i++) {
+      await this.queue[i]();
+    }
+  }
+}
+
+function machine(name) {
+  return new Action(name);
+}
