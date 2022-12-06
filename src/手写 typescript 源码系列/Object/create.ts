214041a57813893988204217ae1e6e3812f4dfaa
R2h1 <1461694018@qq.com>
2022-12-06 18:12:03 +0800

    feat: 完善 myAssign方法

--- src/手写 typescript 源码系列/Object/create.ts
+++ src/手写 typescript 源码系列/Object/create.ts
@@ -3,11 +3,15 @@
  * @param p
  * @returns
  */
-export function myCreate(p: any) {
-  if (typeof p !== 'object') {
+export function myCreate(proto: object | null, propertiesObject?: PropertyDescriptorMap & ThisType<any>) {
+  if (typeof proto !== 'object') {
     throw new TypeError('proto must be a object or null');
   }
   const F = <any>function () {};
-  F.prototype = p;
-  return new F();
+  F.prototype = proto;
+  const obj = new F();
+  if (propertiesObject !== undefined) {
+    Object.defineProperties(obj, propertiesObject);
+  }
+  return obj;
 }
