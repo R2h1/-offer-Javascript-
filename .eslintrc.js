5ba19d614af7655c5a65b3b225b4602f1a514505
R2h1 <1461694018@qq.com>
2023-03-29 20:21:02 +0800

    feat: 添加圈复杂度

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,5 +37,6 @@ module.exports = {
     '@typescript-eslint/naming-convention': 0,
     'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
     'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+    'complexity': ['error', 10]
   },
 };
