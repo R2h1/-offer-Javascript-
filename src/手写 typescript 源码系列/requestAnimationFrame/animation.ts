34cc515b61acbf59c20d908d12f3b0e449552380
R2h1 <1461694018@qq.com>
2023-04-02 17:19:46 +0800

    feat: css时钟

--- src/手写 typescript 源码系列/requestAnimationFrame/animation.ts
+++ src/手写 typescript 源码系列/requestAnimationFrame/animation.ts
@@ -3,27 +3,40 @@
  *      timing: 时间函数，传入一个已过去的时间与总时间之比的小数（0 代表开始，1 代表结束），返回动画完成度（0 代表开始，1 代表结束）
  *      draw: 绘制函数，传入动画完成度（0 代表开始，1 代表结束），并绘制
  *      duration: 动画总时间
+ *      isInfinite: 是否无限动画
  */
 function animation({
   timing,
   draw,
   duration,
+  isInfinite = false,
+  delay = 0,
 }: {
   timing: (pass: number) => number;
   draw: (progress: number) => void;
   duration: number;
+  isInfinite: boolean;
+  delay: number;
 }) {
   let start = performance.now();
   requestAnimationFrame(function animate(time) {
+    const advanceTime = delay < 0 ? -delay * duration : 0;
     // timeFraction 从 0 增加到 1
-    let timeFraction = (time - start) / duration;
-    if (timeFraction > 1) timeFraction = 1;
+    let timeFraction = ((time + advanceTime - start) % duration) / duration;
+    // 有限动画且已过去时间超过总时间
+    if (!isInfinite && timeFraction > 1) {
+      timeFraction = 1;
+    }
     // 计算当前动画状态
     const progress = timing(timeFraction);
 
     draw(progress); // 绘制
 
-    if (timeFraction < 1) {
+    if (isInfinite) {
+      // 无限动画
+      requestAnimationFrame(animate);
+    } else if (timeFraction < 1) {
+      // 有限动画
       requestAnimationFrame(animate);
     }
   });
--- src/效果/时钟/js/index.js
+++ src/效果/时钟/js/index.js
@@ -15,3 +15,41 @@ function getCurrentTime() {
 }
 
 initClock();
+
+function animation({ timing, draw, duration, isInfinite, delay }) {
+  let start = performance.now();
+  requestAnimationFrame(function animate(time) {
+    // 动画提前时间
+    const advanceTime = delay < 0 ? -delay * duration : 0;
+    // timeFraction 从 0 增加到 1
+    let timeFraction = ((time + advanceTime - start) % duration) / duration;
+    // 有限动画且已过去时间超过总时间
+    if (!isInfinite && timeFraction > 1) {
+      timeFraction = 1;
+    }
+    // 计算当前动画状态
+    const progress = timing(timeFraction);
+
+    draw(progress); // 绘制
+
+    if (isInfinite) {
+      // 无限动画
+      requestAnimationFrame(animate);
+    } else if (timeFraction < 1) {
+      // 有限动画
+      requestAnimationFrame(animate);
+    }
+  });
+}
+
+animation({
+  timing: (x) => x,
+  draw: (progress) => {
+    const value = Math.floor(progress * 60);
+    const counter = document.querySelector('.counter');
+    counter.textContent = '秒: ' + String(value).padStart(2, '0');
+  },
+  delay: -(new Date().getSeconds() / 60),
+  duration: 60000,
+  isInfinite: true,
+});
