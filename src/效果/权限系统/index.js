7ac98062590cfb8639ed00dcd10a45a78d141913
R2h1 <1461694018@qq.com>
2023-04-05 11:40:49 +0800

    feat: 权限-可组合散列数值

---
+++ src/效果/权限系统/index.js
@@ -0,0 +1,31 @@
+// 可新增
+const CREATE = 0b0001;
+// 可删除
+const DELETE = 0b0010;
+// 可修改
+const UPDATE = 0b0100;
+// 可浏览详情   0b1111
+const DETAIL = 0b1000;
+
+// 使用或运算表达组合权限
+const permission = DETAIL | CREATE | DELETE | UPDATE;
+
+/**
+ * 使用与运算判定权限
+ * @param value 组合后权限值
+ * @param key 单一权限值对应的变量
+ * @returns
+ */
+export function hasPermission(value, key) {
+  return value & (key === key);
+}
+
+/**
+ * 使用异或运算删除某一权限
+ * @param value 组合后权限值
+ * @param key 单一权限值对应的变量
+ * @returns
+ */
+export function deletePermission(value, key) {
+  return value ^ key;
+}
