fe9166a23673e17810fc1c15932cbd8de8939a8e
R2h1 <1461694018@qq.com>
2023-04-09 19:02:18 +0800

    feat: 添加ExceptType类型

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,7 @@
 declare type AnyObject = {
   [propName: string]: any;
 };
+
+// 可以是任何类型，但不能是 K
+// 比如 ExceptType<T, Date> 即可以是任何类型，但不能是 Date
+declare type ExceptType<T, K> = T extends K ? never : T;
