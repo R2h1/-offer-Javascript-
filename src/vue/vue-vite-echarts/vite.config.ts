ceaabcf55c0318258c61163a4581db980810ae33
R2h1 <1461694018@qq.com>
2023-04-10 09:57:11 +0800

    feat: 添加unplugin-auto-import

--- src/vue/vue-vite-echarts/vite.config.ts
+++ src/vue/vue-vite-echarts/vite.config.ts
@@ -3,10 +3,21 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      // 本地的导入查找位置
+      dirs: ['./src/api'],
+      // 添加类型定义
+      dts: './src/types/auto-import.d.ts',
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
