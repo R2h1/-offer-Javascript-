a2f7f63b5d830fe6b49de7b0a8165738ad63ed4f
R2h1 <1461694018@qq.com>
2023-04-10 23:09:35 +0800

    feat: 使用自定义ref实现防抖ref

---
+++ src/手写 typescript 源码系列/自定义ref实现防抖/debounce.ts
@@ -0,0 +1,26 @@
+// @ts-ignore
+import { customRef } from 'vue';
+
+export function debounceRef<T>(value: T, delay = 1000) {
+  let timer: NodeJS.Timeout | null = null;
+  return customRef((track: Function, trigger: Function) => {
+    return {
+      get() {
+        track();
+        return value;
+      },
+      set(val: T) {
+        if (timer) {
+          clearTimeout(timer);
+          timer = null;
+        }
+        timer = setTimeout(() => {
+          timer = null;
+          // 派发更新
+          value = val;
+          trigger();
+        }, delay);
+      },
+    };
+  });
+}
