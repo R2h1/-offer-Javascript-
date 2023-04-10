810620ed8fd0065bad36e3b3fd2461814a381ebb
R2h1 <1461694018@qq.com>
2023-04-10 15:21:06 +0800

    feat: 添加 live binding

--- src/手写 typescript 源码系列/ESM/2.mjs
+++ src/手写 typescript 源码系列/ESM/2.mjs
@@ -1,4 +1,4 @@
-// esm 中 导出的变量是符号绑定或符号连接：即值引用而不是值拷贝（commonJS）
+// esm 中 导出的变量是符号绑定或符号连接或实时绑定（live binding）：即值引用而不是值拷贝（commonJS）
 // CommonJS 模块是运行时加载，ESM是编译时输出接口
 import { counter, increase } from './1.mjs'
 console.log('counter改变前: ', counter);
