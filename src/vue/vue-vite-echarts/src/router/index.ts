7a20293ead3ade22e2e17bb066d95ddaf6b59a68
R2h1 <1461694018@qq.com>
2023-04-11 21:55:07 +0800

    feat: vite-vue中添加glob自动化导入

--- src/vue/vue-vite-echarts/src/router/index.ts
+++ src/vue/vue-vite-echarts/src/router/index.ts
@@ -1,23 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+
+const pageModules = import.meta.glob('../views/**/page.js', {
+  eager: true,
+  import: 'default',
+});
+
+const componentModules = import.meta.glob('../views/**/index.vue');
+
+const routes = Object.entries(pageModules).map(([pagePath, config]) => {
+  const path = pagePath.replace('../views', '').replace('/page.js', '') || '/';
+  const componentPath = pagePath.replace('/page.js', 'index.vue');
+  const name = path.split('/').filter(Boolean).join('-') || 'index';
+  return {
+    path: path,
+    name: name,
+    component: componentModules[componentPath],
+    meta: config,
+  };
+});
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  routes,
 });
 
 export default router;
