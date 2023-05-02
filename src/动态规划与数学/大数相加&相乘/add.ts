f2f6b1ec38164077c4ce9502eb84b1b505d81d6d
R2h1 <1461694018@qq.com>
2023-05-02 16:58:14 +0800

    feat: 进位变量名更加语义化

--- src/动态规划与数学/大数相加&相乘/add.ts
+++ src/动态规划与数学/大数相加&相乘/add.ts
@@ -18,17 +18,17 @@ export function addBigDigit(num1: string, num2: string, every = 15): string {
   let i = num1.length;
   let j = num2.length;
   let res = '';
-  let curry = 0;
+  let carry = 0;
   // 如果传递的 every 小于 1 或 大于 15，说明参数有问题，则使用默认值 15
   every = every > 15 || every < 1 ? 15 : every;
   while (i > 0 || j > 0) {
     const subOfNum1 = num1.substring(i - every, i);
     const subOfNum2 = num2.substring(j - every, j);
-    const sum = String(Number(subOfNum1) + Number(subOfNum2) + curry);
+    const sum = String(Number(subOfNum1) + Number(subOfNum2) + carry);
     // 当前部分相加结果位数超过指定位数: 15
     if (sum.length > every) {
       res = sum.substring(1) + res;
-      curry = 1;
+      carry = 1;
     } else {
       if (i < every && j < every) {
         res = sum + res;
@@ -36,10 +36,10 @@ export function addBigDigit(num1: string, num2: string, every = 15): string {
         // 如果存在一个加数的位数等于15位，这部分的结果如果不够15需要补0
         res = sum.padStart(every, '0') + res;
       }
-      curry = 0;
+      carry = 0;
     }
     j = j - every;
     i = i - every;
   }
-  return curry === 1 ? curry + res : res;
+  return carry === 1 ? carry + res : res;
 }
