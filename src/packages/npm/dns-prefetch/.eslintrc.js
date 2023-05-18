0d14c1bf96a2ac9435568f29855a29ef28700f5e
R2h1 <1461694018@qq.com>
2023-05-18 17:43:37 +0800

    feat: init

---
+++ src/packages/npm/dns-prefetch/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: 'standard-with-typescript',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'space-before-function-paren': ['error', 'never'],
+  },
+};
