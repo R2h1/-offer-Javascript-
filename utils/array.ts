5edb035e50dc21b8436485b4c1d1b54c9f562ca1
R2h1 <1461694018@qq.com>
2023-05-25 19:13:01 +0800

    feat: 移除一些数组方法

--- utils/array.ts
+++ utils/array.ts
@@ -1,13 +1,3 @@
-// 数组头部插入 newArr = [element].concat(arr);
-
-// 数组尾部插入 newArr = arr.concat([element])
-
-// 数组转换为对象 obj = { ...arr } 注意是浅转换
-
-// 随机取数组值: value = arr[Math.floor(Math.random() * arr.length)]
-
-// 遍历数组： Array.from 或 array.map
-
 /**
  * 将值转化为数组
  * @param value
@@ -74,98 +64,11 @@ const filterAdvanced = <T, _>(arr: T[], condition: (i: T) => Boolean, type: 'inc
   return arr.filter(newCondition);
 };
 
-/**
- * 数组随机化
- * @param arr
- * @returns
- */
-const shuffle = <T, _>(arr: T[]): T[] => arr.sort(() => 0.5 - Math.random());
-
-/**
- * 按key排序
- * @param arr
- * @param key
- * @returns
- */
-const sortBy = <T extends Record<string, any>, K extends keyof T>(arr: T[], key: K): T[] => (
-  arr.concat().sort((a, b) => {
-    if (a[key] > b[key]) {
-      return 1;
-    }
-    if (a[key] < b[key]) {
-      return  -1;
-    }
-    return 0;
-  })
-);
-
-/**
- * 数组排序
- * @param arr
- * @returns
- */
-const sort = (arr: number[]): number[] => arr.sort((a, b) => a - b);
-
-/**
- * 数组拆分成块
- * @param arr
- * @param size
- * @returns
- */
-const chunk = <T,>(arr: T[], size: number): T[][] => (
-  arr.reduce((acc, e, i) => (i % size ? acc[acc.length - 1].push(e) : acc.push([e]), acc), [] as T[][])
-);
-
-/**
- * 矩阵转置
- * @param matrix
- * @returns
- */
-const transpose = <T,>(matrix: T[][]): T[][] => matrix[0].map((col, c) => matrix.map((row, r) => matrix[r][c]));
-
-/**
- * 交互 i 和 j的 值
- * @param a
- * @param i
- * @param j
- * @returns
- */
-const swapItems = <T, _>(
-  a: T[],
-  i: number,
-  j: number
-): T[] => (a[i] && a[j] && [...a.slice(0, i), a[j], ...a.slice(i + 1, j), a[i], ...a.slice(j + 1)]) || a;
-
-/**
- * 数组 unzip
- * @param arr
- * @returns
- */
-const unzip = arr => arr.reduce(
-  (acc, c) => (c.forEach((v, i) => acc[i].push(v)), acc),
-  Array.from({ length: Math.max(...arr.map(a => a.length)) }, _ => [])
-);
-
-/**
- * 数组 zip
- * @param arr
- * @returns
- */
-const zip = (...arr) => Array.from({ length: Math.max(...arr.map(a => a.length)) }, (_, i) => arr.map(a => a[i]));
-
 export {
   castArray,
   isEmpty,
   clone,
   filterAdvanced,
-  shuffle,
-  sortBy,
-  sort,
-  chunk,
-  transpose,
-  swapItems,
-  zip,
-  unzip,
   complement,
 };
 
