36c5a01271fac8141f799c221af47d8df8a5e420
R2h1 <1461694018@qq.com>
2023-07-19 18:56:56 +0800

    feat: 对函数addImpl添加类型标注

--- src/手写 typescript 源码系列/对函数进行类型标注/watch.ts
+++ src/手写 typescript 源码系列/对函数进行类型标注/watch.ts
@@ -40,3 +40,27 @@ const personWatcher = watch({
 personWatcher.on('ageChanged', (oldValue, newValue) => {
   console.log(oldValue, newValue);
 });
+
+/**
+  // 给函数进行类型标注
+  // 最后一个参数是函数，前面是不定参数
+  // 不定参数的个数等于最后一个函数的参数个数
+  // 不定参数的值对应于最后一个函数的参数类型
+  addImpl('number', 'boolean', 'number', (a, b, c) => {})
+ */
+type JSTypeMap = {
+  string: string;
+  number: number;
+  boolean: boolean;
+  object: object;
+  function: Function;
+  symbol: symbol;
+  undefined: undefined;
+  bigint: bigint;
+};
+type JSTypeName = keyof JSTypeMap;
+type ArgsType<T extends JSTypeName[]> = {
+  // [I in keyof T]、T[I]对于数组来说分别就是下标和值
+  [I in keyof T]: JSTypeMap[T[I]];
+};
+declare function addImpl<T extends JSTypeName[]>(...args: [...T, (...args: ArgsType<T>) => any]): void;
