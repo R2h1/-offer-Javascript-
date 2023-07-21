3c30f0993deca6fd21e30c2f60d90e77d9e0f2d5
R2h1 <1461694018@qq.com>
2023-07-21 23:30:37 +0800

    feat: 添加 js 实现函数重载工具方法

--- src/手写 typescript 源码系列/JS函数重载实现/addMethod.ts
+++ src/手写 typescript 源码系列/JS函数重载实现/addMethod.ts
@@ -1,5 +1,9 @@
 /**
- * js 实现函数重载
+ * js 实现函数重载(jquery里的实现)
+ * 缺点：
+ *   1. 需要创建一个对象 object 并传入
+ *   2. fn.length 会排除掉参数默认值
+ *   3. 只能适配参数的数量，对于参数类型无法适配
  * @param object
  * @param name
  * @param fn
@@ -15,4 +19,28 @@ export function addMethod(object: AnyObject, name: string, fn: Function) {
       return old.apply(this, args);
     }
   };
+}
+
+/** */
+export function createOverload() {
+  const callMap = new Map();
+  /** 实际调用的重载函数 */
+  function overload(this: any, ...args: any[]) {
+    const key = args.map((arg) => typeof arg).join(','); // 实参类型
+    const fn = callMap.get(key);
+    if (fn) {
+      return fn.apply(this, args);
+    }
+    throw new Error('no matching function');
+  }
+  /** 添加重载函数的工具函数 */
+  overload.addImp = function (...args: any[]) {
+    const fn = args.pop();
+    if (typeof fn !== 'function') {
+      return;
+    }
+    const types = args;
+    callMap.set(types.join(','), fn);
+  };
+  return overload;
 }
