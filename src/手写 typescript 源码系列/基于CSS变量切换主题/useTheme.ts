6de7d15adef92e75475b52aca4feb379fb1f8988
R2h1 <1461694018@qq.com>
2023-07-22 13:15:34 +0800

    feat: 添加跟随系统的实现

--- src/手写 typescript 源码系列/基于CSS变量切换主题/useTheme.ts
+++ src/手写 typescript 源码系列/基于CSS变量切换主题/useTheme.ts
@@ -1,13 +1,27 @@
 import { ref, watchEffect } from 'vue';
 
 const LOCAL_KEY = '__theme__';
-type Theme = 'light' | 'dark';
+type Theme = 'light' | 'dark' | 'OS';
 
 const theme = ref<Theme>((window.localStorage.getItem(LOCAL_KEY) as Theme) || 'light');
+const match = matchMedia('(prefers-color-scheme: dark)');
+
+function followOS() {
+  if (match.matches) {
+    document.documentElement.dataset.theme = 'dark';
+  } else {
+    document.documentElement.dataset.theme = 'light';
+  }
+}
 
 watchEffect(() => {
-  document.documentElement.dataset.theme = theme.value;
   localStorage.setItem(LOCAL_KEY, theme.value);
+  if (theme.value === 'OS') {
+    followOS();
+    match.addEventListener('change', followOS);
+  } else {
+    document.documentElement.dataset.theme = theme.value;
+  }
 });
 
 export default function useTheme() {
