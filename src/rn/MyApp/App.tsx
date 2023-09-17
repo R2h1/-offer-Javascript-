754ac576bdd71c63a692b5d5942b6991ff5af633
R2h1 <1461694018@qq.com>
2023-09-17 19:54:29 +0800

    feat: init RN WorthApp

--- src/rn/MyApp/App.tsx
+++ src/rn/MyApp/App.tsx
@@ -70,7 +70,7 @@ function App(): JSX.Element {
           }}
         >
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come back to see your
+            编辑 <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come back to see your
             edits.
           </Section>
           <Section title="See Your Changes">
