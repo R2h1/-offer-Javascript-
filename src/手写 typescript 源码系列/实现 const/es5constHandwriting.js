2b610d212d3debe6c304e218da0eb3731a52c0ec
R2h1 <1461694018@qq.com>
2023-12-17 00:25:21 +0800

    fix: const 手写

--- src/手写 typescript 源码系列/实现 const/es5constHandwriting.js
+++ src/手写 typescript 源码系列/实现 const/es5constHandwriting.js
@@ -19,34 +19,29 @@ Object.defineProperty(obj, prop, desc)
 
 */
 
-var __const = function (data, value) {
-  window.data = value; // 把要定义的data挂载到window下，并赋值value
-  Object.defineProperty(window, data, {
-    // 利用Object.defineProperty的能力劫持当前对象，并修改其属性描述符
+var __const = function (variable, value) {
+  global[variable] = value; // 把要定义的 data 挂载到 window 下，并赋值 value
+  Object.defineProperty(global, variable, {
+    // 利用 Object.defineProperty 的能力劫持当前对象，并修改其属性描述符
     enumerable: false,
     configurable: false,
     get: function () {
       return value;
     },
-    set: function (data) {
-      if (data !== value) {
-        // 当要对当前属性进行赋值时，则抛出错误！
-        throw new TypeError('Assignment to constant variable.');
-      } else {
-        return value;
-      }
-    },
+    set: function (value) {
+      throw new TypeError('Assignment to constant variable.');
+    }
   });
 };
 __const('a', 10);
 console.log(a);
-delete a;
+delete global.a;
 console.log(a);
-for (let item in window) {
-  // 因为const定义的属性在global下也是不存在的，所以用到了enumerable: false来模拟这一功能
+for (let item in global) {
+  // 因为 const 定义的属性在global下也是不存在的，所以用到了enumerable: false来模拟这一功能
   if (item === 'a') {
     // 因为不可枚举，所以不执行
-    console.log(window[item]);
+    console.log(global[item]);
   }
 }
-a = 20; // 报错
+a = 10; // 报错
